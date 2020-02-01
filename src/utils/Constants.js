b1ded6a2b735e59bb94f10e95d51bb3edb8b26c3
Mike Austin <maustinfl@gmail.com>
2020-02-01 09:53:45 +0000

    Updated prod url to point to gars-api subdomain

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,2 +1,2 @@
 // TODO: this needs to be part of build process to point to local/dev or production
-export const API_URL = process.env.NODE_ENV == 'production' ? 'https://lcars.maustin.io/v1' : 'http://localhost:11011/v1';
+export const API_URL = process.env.NODE_ENV == 'production' ? 'https://gars-api.maustin.io/v1' : 'http://localhost:11011/v1';
