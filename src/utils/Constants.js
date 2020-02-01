60eea6b126a56fb7a2d59fe180c21e5234b8e766
Michael Austin <maustin@accesso.com>
2020-02-01 02:20:41 -0800

    removed comment

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,2 +1,1 @@
-// TODO: this needs to be part of build process to point to local/dev or production
 export const API_URL = process.env.NODE_ENV == 'production' ? 'https://gars-api.maustin.io/v1' : 'http://localhost:11011/v1';
